83554ca6c61e0fcf3f4548580a982311a8ac2548
AlexPirciu <AlexPirciu@github.com>
2018-07-09 14:36:12 +0300

    Created the DBContext
    Created the Context for the "Authors" and "Books" entities.

---
+++ RESTful_API_ASP.NET_Core/RESTful_API_ASP.NET_Core/Entities/LibraryContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RESTful_API_ASP.NET_Core.Entities
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
